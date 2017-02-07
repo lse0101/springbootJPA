2381840a3390e866f3317991ae29db0fce85a514
lse <lse0101@gmail.com>
2017-02-07 18:13:12 +0900

    tymeleaf layout 적용 및 flyway 적용

--- src/main/java/com/crazysalaryman/AppConfig.java
+++ src/main/java/com/crazysalaryman/AppConfig.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 import javax.sql.DataSource;
@@ -55,14 +57,17 @@ public class AppConfig {
     log.info(url);
     log.info(username);
     log.info(password);
+    log.info(this.dataSourceProperties.getDriverClassName());
 
     DataSourceBuilder factory = DataSourceBuilder
             .create(dataSourceProperties.getClassLoader())
+            .driverClassName(this.dataSourceProperties.getDriverClassName())
             .url(url)
             .username(username)
             .password(password);
 
     log.info("######################################################################################");
+//    return factory.build();
     return new Log4jdbcProxyDataSource(factory.build());
   }
 
