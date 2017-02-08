a4584fcbcdf9ce6502b4525dc81830dd6a5b3dc1
lse <lse0101@gmail.com>
2017-02-08 10:16:42 +0900

    add untracked file

--- build/resources/main/db/migration/V1__create_schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE customers (
-  id SERIAL PRIMARY KEY,
-  first_name VARCHAR (30),
-  last_name VARCHAR(30)
-);
--- build/resources/main/db/migration/V2__import-initial-data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO customers(first_name, last_name) values('Nobita', 'Nobi');
-INSERT INTO customers(first_name, last_name) values('Takasi', 'Goda');
-INSERT INTO customers(first_name, last_name) values('Seuneo', 'Honekawa');
-INSERT INTO customers(first_name, last_name) values('Shizuka', 'Minamoto');
